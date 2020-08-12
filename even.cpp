e84cfb391e2ad90e8e3e304209f20d3e673f3e0b
Rumman1 <rummankhan1745@gmail.com>
2020-08-12 15:58:39 +0500

    even

---
+++ even.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main ()
+{ 
+int array [i] ;
+ int even=0 ;
+ int odd=0 ;
+cin>> "Please enter your numbers";
+cout << array [i];
+for ( int j=1 ; j<=array [i] ;j++)
+{
+    if (array [i]%2==0)
+    {
+cout<< even++;
+}
+else 
+cout << odd++;
+return 0;
+}
+}
--- file.cpp
+++ file.cpp
@@ -8,6 +8,5 @@ int main()
 for ( int i=5 ; i>=0 ; i--)
 {cout << Name [i];
 
-}
     return 0;
 }
