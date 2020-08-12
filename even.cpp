02f3042ec6971cf5c28df97dddaa88751e225b19
Rumman1 <rummankhan1745@gmail.com>
2020-08-12 16:33:29 +0500

    even

--- even.cpp
+++ even.cpp
@@ -3,11 +3,15 @@ using namespace std;
 int main ()
 { 
  int i;
+ int n;
  int array[i] ;
  int even=0 ;
  int odd=0 ;
-cin>> "Please enter your numbers";
-cout << array [i];
+cout << "Please enter total numbers";
+cin >> n; 
+for (i=0 ; i<n ;i++ )
+{
+cin >> array [i];
 for ( int j=1 ; j<=array [i] ;j++)
 {
     if (array [i]%2==0)
@@ -16,6 +20,8 @@ cout<< even++;
 }
 else 
 cout << odd++;
+}
+}
 return 0;
 }
-}
+
